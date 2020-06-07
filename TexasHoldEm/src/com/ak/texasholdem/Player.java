daede5670d12eac18014de76d6f4e06ef2914aab
Loredy <ferdini.97.16@gmail.com>
2020-06-08 00:36:06 +0200

    Player Class

--- TexasHoldEm/src/com/ak/texasholdem/Player.java
+++ TexasHoldEm/src/com/ak/texasholdem/Player.java
@@ -2,8 +2,8 @@ package com.ak.texasholdem;
 
 public class Player {
 	
-	public String name;
-	public long cash;
+	private String name;
+	private long cash;
 	
 	private String loginName;
 	private String password;
@@ -22,52 +22,29 @@ public class Player {
 		this.cash = cash;
 	}
 
-	public String getName() {
-		return name;
-	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getPassword() {
-		return password;
+	public void setCash(long cash) {
+		this.cash = cash;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public long getCash() {
-		return cash;
-	}
-
-	public void setCash(long cash) {
-		this.cash += cash;
-	}
-
-	public Card getCard1() {
-		return card1;
-	}
-
 	public void setCard1(Card card1) {
 		this.card1 = card1;
-	}
-
-	public Card getCard2() {
-		return card2;
 	}
 
 	public void setCard2(Card card2) {
 		this.card2 = card2;
 	}
 
-	public String getLoginName() {
-		return loginName;
-	}
-
-	public void setLoginName(String loginName) {
-		this.loginName = loginName;
-	}
 
 }
