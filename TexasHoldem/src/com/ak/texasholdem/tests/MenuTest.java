c913ed6801d3a3c96ddcfbdb9ff654e847c871f4
Bogdanov Vladimir <bogdanov@t-email.hu>
2020-06-21 21:48:20 +0200

    JUnit5, tests

--- TexasHoldem/src/com/ak/texasholdem/tests/MenuTest.java
+++ TexasHoldem/src/com/ak/texasholdem/tests/MenuTest.java
@@ -1,16 +1,48 @@
 package com.ak.texasholdem.tests;
 
-public class MenuTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-	/**
-	 * it is only tests
-	 */
-	public static void main(String[] args) {
-	//	Session session = new Session();
-	//	session.run();
-		
-		System.out.println();
-		
+import org.junit.jupiter.api.Test;
+
+import com.ak.texasholdem.menu.Menu;
+import com.ak.texasholdem.menu.MenuPoint;
+
+public class MenuTest {
+	private Menu menu = new Menu("bejelentkezés");
+
+	@Test
+	void givenMenu_WhenGivenNegativeNumber_ThenReturnWithErrorMenuPoint() {
+		// given
+
+		// when
+		int input = -1;
+		MenuPoint actual = menu.getMenuPoint(input);
+		// then
+		assertEquals(MenuPoint.ERROR, actual);
+	}
+
+	@Test
+	void givenMenu_WhenGivenNumberBiggerThenMenuPoint_ThenReturnWithErrorMenuPoint() {
+		// given
+
+		// when
+		int input = 100;
+		MenuPoint actual = menu.getMenuPoint(input);
+		// then
+		assertEquals(MenuPoint.ERROR, actual);
+
+	}
+
+	@Test
+	void givenMenu_WhenGivenNumberOfCorrectMenuPoint_ThenReturnWithNotErrorMenuPoint() {
+		// given
+
+		// when
+		int input = 1;
+		MenuPoint actual = menu.getMenuPoint(input);
+		// then
+		assertNotEquals(MenuPoint.ERROR, actual);
 	}
 
 }
