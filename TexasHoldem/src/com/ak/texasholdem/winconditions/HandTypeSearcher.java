b32001b9a83f5a07e48c94000486fb493f912156
Bogdanov Vladimir <bogdanov@t-email.hu>
2020-07-21 20:27:24 +0200

    1

--- TexasHoldem/src/com/ak/texasholdem/winconditions/HandTypeSearcher.java
+++ TexasHoldem/src/com/ak/texasholdem/winconditions/HandTypeSearcher.java
@@ -2,10 +2,12 @@ package com.ak.texasholdem.winconditions;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.ak.texasholdem.cards.Card;
+import com.ak.texasholdem.cards.Rank;
+import com.ak.texasholdem.cards.Suit;
 
 public abstract class HandTypeSearcher {
 	protected List<Card> cards;
@@ -13,31 +15,56 @@ public abstract class HandTypeSearcher {
 
 	public HandTypeSearcher(List<Card> cards) {
 		this.cards = cards;
-		
+		sortCardsByRank(this.cards);
 	}
 
-	protected List<Card> straightSearcher(int fromInclusive, int toInclusive) {
-		List<Card> bestCards = new ArrayList<>();
-		for (int value = fromInclusive; value >= toInclusive; value--) {
-			for (Card card : cards) {
-				if (card.getRank()
-						.getRankValue() == value) {
-					bestCards.add(card);
-					break;
-				} else {
-					return null;
+	public void sortCardsByRank(List<Card> cards) {
+		Comparator c = new Comparator<Card>() {
+
+			@Override
+			public int compare(Card card1, Card card2) {
+				if (card1.getRank()
+						.getRankValue() < card2.getRank()
+								.getRankValue()) {
+					return 1;
 				}
+				if (card1.getRank()
+						.getRankValue() > card2.getRank()
+								.getRankValue()) {
+
+					return -1;
+				}
+				return 0;
 			}
-		}
-		return bestCards;
+		};
+		Collections.sort(cards, c);
 	}
 
 	public List<Card> getBestCards() {
 		return bestCards;
 	}
 
+	protected boolean hasSpecificRank(Rank rank) {
+		for (Card card : cards) {
+			if (card.getRank()
+					.equals(rank)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected int countSuits(Suit suit) {
+		int counter = 0;
+		for (Card card : cards) {
+			if (card.getSuit()
+					.equals(suit)) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
 	public abstract HandTypes search();
-	
-	
 
 }
